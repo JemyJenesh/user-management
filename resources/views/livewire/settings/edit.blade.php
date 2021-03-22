2735452446fe1dc2586c75b57be51d739193cfd8
JemyJenesh <xemejenesh@gmail.com>
2021-03-22 10:55:44 +0545

    settings also done

--- resources/views/livewire/settings/edit.blade.php
+++ resources/views/livewire/settings/edit.blade.php
@@ -3,5 +3,4 @@
     <input wire:click="changeValue" id="multiple-user-roles" type="checkbox" class="custom-control-input" @if ($value == 1) checked @endif>
     <label for="multiple-user-roles" class="custom-control-label">Allow multiple user roles</label>
   </div>
-  {{ $value }}
 </form>
